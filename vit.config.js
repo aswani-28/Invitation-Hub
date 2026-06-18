85e8b174da2f153ad0cf879a7020855645f02979
Aswani <aswanikunda3.gmail.com>
2026-06-18 10:05:59 +0100

    fix vite

--- vit.config.js
+++ vit.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Invitation-Hub/",
+  base:'/Invitation-Hub/',
   plugins: [react()],
  
 })
